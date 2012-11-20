24a39c4703b8b43b8f995684764848ded79587f3
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-11-20 15:46:47 -0600

    tidying up for base off howtonode

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 /* deps */
 
 var http = require('http')
-  , app = require('./app')
+  , Stack = require('stack')
+  , Creationix = require('creationix')
   , port = parseInt(process.env.PANCAKE_IO_PORT, 10) || 3000;
+  //, app = require('./app')
 
   // process title
   //
@@ -12,13 +14,21 @@ process.title = 'pancake';
 // create the HTTP server
 //
 
-var server = http.createServer(app);
+//var server = http.createServer(app);
+
+
+// create stack server
+
+http.createServer(Stack(
+  Creationix.log(),
+  require('wheat')(__dirname)
+  )).listen(3000);
 
 // Listen.
 //
 
-server.listen(port, function() {
-  port = server.address().port;
-  console.log('pancake %j server listening on port %d', app.settings.env, port);
-  console.log('libgit2 version %j', app.settings.version);
-});
+//server.listen(port, function() {
+//  port = server.address().port;
+//  console.log('pancake %j server listening on port %d', app.settings.env, port);
+//  console.log('libgit2 version %j', app.settings.version);
+//});
