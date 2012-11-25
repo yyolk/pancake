1c725fc0c41dd7901957537ef226454d236d9fea
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-11-25 15:46:20 -0600

    git ffi

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ var server = http.createServer(app);
 server.listen(port, function() {
   port = server.address().port;
   console.log('pancake %j server listening on port %d', app.settings.env, port);
-  console.log('libgit2 version %j', app.settings.version);
+  //console.log('libgit2 version %j', app.settings.version);
 });
